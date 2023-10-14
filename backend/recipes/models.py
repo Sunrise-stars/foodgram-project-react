c9cf8a1b6f95099935b7032f2c72302c6c013260
Юрий Рыбалко <rastiwka22@bk.ru>
2023-10-15 00:08:59 +0700

    Flake8

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -82,4 +82,5 @@ class RecipeIngredient(models.Model):
         ]
 
     def __str__(self):
-        return f"{self.ingredient.name} ({self.amount} {self.ingredient.measurement_unit})"
+        return f"{self.ingredient.name} ({self.amount} 
+        {self.ingredient.measurement_unit})"
