e4cf03c4274145d8649363c6116fefd44bbb6894
Юрий Рыбалко <rastiwka22@bk.ru>
2023-10-16 15:19:34 +0700

    99 reviy

--- backend/api/mixins.py
+++ backend/api/mixins.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
-from .models import Recipe  
+from .models import Recipe
+
 
 class AddRemoveFromListMixin:
     def perform_action(
